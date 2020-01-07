b8dc2c745bb41ed95ee5754166011ee4dc02c14b
hedo5 <matej.hedzet@gmail.com>
2020-01-07 13:57:26 +0100

    Add files via upload

---
+++ Projektnizadatak2(PHPiMysql)/kontakt.php
@@ -0,0 +1,72 @@
+<?php
+print '
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+	    <link rel="stylesheet" href="new1.css">
+		<title>360 fotografija</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Matej Hedzet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	</head>
+	
+<body>
+<header>
+		<div class="slika-360"></div>
+		<nav>
+			<ul>
+			  <li><a href="index.php">Početna stranica</a></li>
+			  <li><a href="novosti.php">Novosti</a></li>
+			  <li><a href="kontakt.php">Kontakt</a></li>
+			  <li><a href="onama.php">O nama</a></li>
+			  <li><a href="galerija.php">Galerija</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h1>Kontakt</h1>
+		<div id="kontakt">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2781.7890741539636!2d15.966758816056517!3d45.795453279106205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d68b5d094979%3A0xda8bfa8459b67560!2sUl.+Vrbik+VIII%2C+10000%2C+Zagreb!5e0!3m2!1shr!2shr!4v1509296660756" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<form action="http://vvg.hr" id="kontakt" name="kontakt" method="POST">
+				<label for="ime">Ime *</label>
+				<input type="text" id="ime" name="ime" placeholder="Vaše me.." required>
+
+				<label for="Prezime">Prezime *</label>
+				<input type="text" id="Prezime" name="Prezime" placeholder="Vaše prezime.." required>
+				
+				<label for="prezime">Vaš E-mail *</label>
+				<input type="email" id="email" name="email" placeholder="Vaš e-mail.." required>
+
+				<label for="drzava">Država</label>
+				<select id="drzava" name="drzava">
+				  <option value="">Please select</option>
+				  <option value="BE">Belgija</option>
+				  <option value="HR" selected>Croatia</option>
+				  <option value="LU">Luxembourg</option>
+				  <option value="HU">Mađarska</option>
+				</select>
+
+				<label for="poruka">Vaša poruka</label>
+				<textarea id="poruka" name="poruka" placeholder="Napišite poruku.." style="height:200px"></textarea>
+
+				<input type="posaljite" value="Pošaljite">
+			</form>
+		</div>
+	</main>
+	
+				<hr>
+			
+			<a href="index.php"><img </a>
+			<h2>Povratak na početnu</h2>
+	
+	<footer>
+		<p>Copyright &copy; 2020 Matej Hedzet. <a href="https://github.com/hedo5/PHP-projekt"><img src="foto/github-octocat.png" title="Github" alt="Github"></a></p>
+	</footer>
+
+</body>
+</html>';
+?>
